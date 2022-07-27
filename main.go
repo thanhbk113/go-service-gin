90c5639c4e3e4d0c672a41614fa38bd13e7019a5
Tran Ngoc Thanh <thanhbk113@gmail.com>
2022-07-27 21:42:42 +0700

    add port

--- main.go
+++ main.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"example/web-service/pkg/models"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
 	router := gin.Default()
 	router.GET("/albums", models.GetAlbums)
 	router.POST("/albums", models.PostAlbum)
 	router.GET("/albums/:id", models.GetAlbumById)
 	router.PUT("/albums/:id", models.UpdateAlbumById)
 	router.DELETE("/albums/:id", models.DeleteAlbums)
-	router.Run("localhost:8080")
+	router.Run("0.0.0.0" + port)
 }
